e9151547ef67658623cb1f57353a8abba21a6297
Potatonator5533 <josue04rosales@gmail.com>
2024-12-12 16:53:01 -0800

    JS update

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,7 @@
+// navigator.geolocation.getCurrentPosition(success, errorFunc);
+
 let weatherImg1 = document.getElementById("weatherImg1");
-let weather1 = document.getElementById("weather1");
+let weather = document.getElementById("weather");
 let DOTW1 = document.getElementById("DOTW1");
 let currentTemp1 = document.getElementById("currentTemp1");
 let wind = document.getElementById("wind");
@@ -31,3 +33,94 @@ let thirdDay = document.getElementById("thirdDay");
 let fourthDay = document.getElementById("fourthDay");
 let fifthDay = document.getElementById("fifthDay");
 let isFavorite = document.getElementById("isFavorite");
+
+// let degreeSetting = "f";
+// let data1;
+
+function test1() {
+  return fetch("../test1.json")
+    .then((response) => response.json())
+    .then((data1) => {
+      return data1;
+    });
+}
+
+function test2() {
+  return fetch("../test2.json")
+    .then((response) => response.json())
+    .then((data2) => {
+      console.log(data2);
+      return data2;
+    });
+}
+
+LoadPage();
+// test2();
+
+async function LoadPage() {
+  // const promise1 = await fetch('https://api.openweathermap.org/data/2.5/weather?lat=38.5810606&lon=-121.493895&appid=2af1c812e6a78735121f5ae9d2d27f6a');
+  // data1 = await promise1.json();
+
+//   const promise2 = await fetch('https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=38.5810606&lon=-121.493895&appid=2af1c812e6a78735121f5ae9d2d27f6a');
+//   data2 = await promise2.json();
+
+  test1().then((data1) => {
+    console.log(data1);
+
+    // Main Card
+    weather.innerText = data1.weather[0].main;
+    // DOTW1.innerText = data.
+    currentTemp1.innerText = TempConverter(data1.main.temp);
+    maxMinTemp1.innerText =
+      TempConverter(data1.main.temp_max) +
+      "°/" +
+      TempConverter(data1.main.temp_min) +
+      "°";
+    wind.innerText = "Wind: " + data1.wind.speed;
+    humidity.innerText = "Humidity " + data1.main.humidity;
+    city.innerText = data1.name + ", " + data1.sys.country;
+  });
+//   test2.then((data2) => {
+    // Next day's forecast
+    for (let i = 0; i < 5; i++) {
+
+    }
+//   });
+}
+
+// alert("hi");
+
+fahrenheit.addEventListener("click", () => {
+  celsius.className = "unselected";
+  fahrenheit.className = "";
+
+  // Main Card
+  currentTemp1.innerText = TempConverter(data1.main.temp);
+  maxMinTemp1.innerText =
+    TempConverter(data1.main.temp_max) +
+    "°/" +
+    TempConverter(data1.main.temp_min) +
+    "°";
+});
+
+celsius.addEventListener("click", () => {
+  celsius.className = "";
+  fahrenheit.className = "unselected";
+
+  // Main Card
+  currentTemp1.innerText = CelsiusConverter(data1.main.temp);
+  maxMinTemp1.innerText =
+    CelsiusConverter(data1.main.temp_max) +
+    "°/" +
+    CelsiusConverter(data1.main.temp_min) +
+    "°";
+});
+
+function TempConverter(kelvin) {
+  return Math.floor(((kelvin - 273.15) * 9) / 5 + 32);
+}
+
+function CelsiusConverter(kelvin) {
+  let fahrenheit = Math.floor(((kelvin - 273.15) * 9) / 5 + 32);
+  return Math.floor(((fahrenheit - 32) * 5) / 9);
+}
